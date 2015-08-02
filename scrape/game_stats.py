b2b4d9e56bd18201b1f6ddc3339b1da6fdba93b1
dblackrun <blackport99@yahoo.com>
2015-08-02 12:17:45 -0400

    fixed shots list

--- scrape/game_stats.py
+++ scrape/game_stats.py
@@ -14,7 +14,7 @@ class GameData:
         self._player_tracking_boxscore_data = json.loads(self._player_tracking_boxscore_response.read())
 
         self._teams = [self.player_tracking_boxscore_team()[0]['TEAM_ID'], self.player_tracking_boxscore_team()[1]['TEAM_ID']]
-        
+
     def pbp(self):
         _headers = self._pbp_data['resultSets'][0]['headers']
         _rows = self._pbp_data['resultSets'][0]['rowSet']
@@ -24,7 +24,7 @@ class GameData:
         _headers = self._player_tracking_boxscore_data['resultSets'][0]['headers']
         _rows = self._player_tracking_boxscore_data['resultSets'][0]['rowSet']
         return [dict(zip(_headers, row)) for row in _rows]
-        
+
     def player_tracking_boxscore_team(self):
         _headers = self._player_tracking_boxscore_data['resultSets'][1]['headers']
         _rows = self._player_tracking_boxscore_data['resultSets'][1]['rowSet']
@@ -32,15 +32,12 @@ class GameData:
 
 
     def shots(self):
-        shots = []
+        _shots = []
         for team in self._teams:
             _shots_url = "http://stats.nba.com/stats/shotchartdetail?Season=2014-15&SeasonType=Regular+Season&LeagueID=00&TeamID="+str(team)+"&PlayerID=0&GameID="+self.game_id+"&Outcome=&Location=&Month=0&SeasonSegment=&DateFrom=&DateTo=&OpponentTeamID=0&VsConference=&VsDivision=&Position=&RookieYear=&GameSegment=&Period=0&LastNGames=0&ContextFilter=&ContextMeasure=FG_PCT&display-mode=performance&zone-mode=zone&zoneOverlays=false&zoneDetails=false&viewShots=true"
             _shots_response = urllib2.urlopen(_shots_url)
             _shots_data = json.loads(_shots_response.read())
             _headers = _shots_data['resultSets'][0]['headers']
             _rows = _shots_data['resultSets'][0]['rowSet']
-            shots.append([dict(zip(_headers, row)) for row in _rows])
-        return shots
-        
-        
-    
+            _shots += [dict(zip(_headers, row)) for row in _rows]
+        return _shots
