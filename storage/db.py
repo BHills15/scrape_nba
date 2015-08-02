f32363a2f3b87e25c8ef9b593668d344e4a7f030
dblackrun <blackport99@yahoo.com>
2015-08-02 15:09:15 -0400

    added all tables for boxscores

--- storage/db.py
+++ storage/db.py
@@ -95,6 +95,155 @@ class Db:
         );'
         cursor.execute(pbp_query)
 
+        shots_query = 'CREATE TABLE IF NOT EXISTS shots\
+        (\
+        GRID_TYPE VARCHAR(255),\
+        GAME_ID VARCHAR(255),\
+        GAME_EVENT_ID INT,\
+        PLAYER_ID VARCHAR(255),\
+        PLAYER_NAME VARCHAR(255),\
+        TEAM_ID VARCHAR(255),\
+        TEAM_NAME VARCHAR(255),\
+        PERIOD INT,\
+        MINUTES_REMAINING INT,\
+        SECONDS_REMAINING INT,\
+        EVENT_TYPE VARCHAR(255),\
+        ACTION_TYPE VARCHAR(255),\
+        SHOT_TYPE VARCHAR(255),\
+        SHOT_ZONE_BASIC VARCHAR(255),\
+        SHOT_ZONE_AREA VARCHAR(255),\
+        SHOT_ZONE_RANGE VARCHAR(255),\
+        SHOT_DISTANCE INT,\
+        LOC_X INT,\
+        LOC_Y INT,\
+        SHOT_ATTEMPTED_FLAG BOOLEAN,\
+        SHOT_MADE_FLAG BOOLEAN,\
+        PRIMARY KEY(GAME_ID, GAME_EVENT_ID)\
+        );'
+        cursor.execute(shots_query)
+
+        advanced_boxscores_query = 'CREATE TABLE IF NOT EXISTS advanced_boxscores\
+        (\
+        GAME_ID VARCHAR(255),\
+        TEAM_ID VARCHAR(255),\
+        TEAM_ABBREVIATION VARCHAR(255),\
+        TEAM_CITY VARCHAR(255),\
+        PLAYER_ID VARCHAR(255),\
+        PLAYER_NAME VARCHAR(255),\
+        START_POSITION VARCHAR(255),\
+        COMMENT VARCHAR(255),\
+        MIN VARCHAR(255),\
+        OFF_RATING DOUBLE,\
+        DEF_RATING DOUBLE,\
+        NET_RATING DOUBLE,\
+        AST_PCT DOUBLE,\
+        AST_TOV DOUBLE,\
+        AST_RATIO DOUBLE,\
+        OREB_PCT DOUBLE,\
+        DREB_PCT DOUBLE,\
+        REB_PCT DOUBLE,\
+        TM_TOV_PCT DOUBLE,\
+        EFG_PCT DOUBLE,\
+        TS_PCT DOUBLE,\
+        USG_PCT DOUBLE,\
+        PACE DOUBLE,\
+        PIE DOUBLE,\
+        PRIMARY KEY(GAME_ID, PLAYER_ID)\
+        );'
+        cursor.execute(advanced_boxscores_query)
+
+        advanced_boxscores_team_query = 'CREATE TABLE IF NOT EXISTS advanced_boxscores_team\
+        (\
+        GAME_ID VARCHAR(255),\
+        TEAM_ID VARCHAR(255),\
+        TEAM_NAME VARCHAR(255),\
+        TEAM_ABBREVIATION VARCHAR(255),\
+        TEAM_CITY VARCHAR(255),\
+        MIN VARCHAR(255),\
+        OFF_RATING DOUBLE,\
+        DEF_RATING DOUBLE,\
+        NET_RATING DOUBLE,\
+        AST_PCT DOUBLE,\
+        AST_TOV DOUBLE,\
+        AST_RATIO DOUBLE,\
+        OREB_PCT DOUBLE,\
+        DREB_PCT DOUBLE,\
+        REB_PCT DOUBLE,\
+        TM_TOV_PCT DOUBLE,\
+        EFG_PCT DOUBLE,\
+        TS_PCT DOUBLE,\
+        USG_PCT DOUBLE,\
+        PACE DOUBLE,\
+        PIE DOUBLE,\
+        PRIMARY KEY(GAME_ID, TEAM_ID)\
+        );'
+        cursor.execute(advanced_boxscores_team_query)
+
+        traditional_boxscores_query = 'CREATE TABLE IF NOT EXISTS traditional_boxscores\
+        (\
+        GAME_ID VARCHAR(255),\
+        TEAM_ID VARCHAR(255),\
+        TEAM_ABBREVIATION VARCHAR(255),\
+        TEAM_CITY VARCHAR(255),\
+        PLAYER_ID VARCHAR(255),\
+        PLAYER_NAME VARCHAR(255),\
+        START_POSITION VARCHAR(255),\
+        COMMENT VARCHAR(255),\
+        MIN VARCHAR(255),\
+        FGM INT,\
+        FGA INT,\
+        FG_PCT DOUBLE,\
+        FG3M INT,\
+        FG3A INT,\
+        FG3_PCT DOUBLE,\
+        FTM INT,\
+        FTA INT,\
+        FT_PCT DOUBLE,\
+        OREB INT,\
+        DREB INT,\
+        REB INT,\
+        AST INT,\
+        STL INT,\
+        BLK INT,\
+        `TO` INT,\
+        PF INT,\
+        PTS INT,\
+        PLUS_MINUS INT,\
+        PRIMARY KEY(GAME_ID, PLAYER_ID)\
+        );'
+        cursor.execute(traditional_boxscores_query)
+
+        traditional_boxscores_team_query = 'CREATE TABLE IF NOT EXISTS traditional_boxscores_team\
+        (\
+        GAME_ID VARCHAR(255),\
+        TEAM_ID VARCHAR(255),\
+        TEAM_NAME VARCHAR(255),\
+        TEAM_ABBREVIATION VARCHAR(255),\
+        TEAM_CITY VARCHAR(255),\
+        MIN VARCHAR(255),\
+        FGM INT,\
+        FGA INT,\
+        FG_PCT DOUBLE,\
+        FG3M INT,\
+        FG3A INT,\
+        FG3_PCT DOUBLE,\
+        FTM INT,\
+        FTA INT,\
+        FT_PCT DOUBLE,\
+        OREB INT,\
+        DREB INT,\
+        REB INT,\
+        AST INT,\
+        STL INT,\
+        BLK INT,\
+        `TO` INT,\
+        PF INT,\
+        PTS INT,\
+        PLUS_MINUS INT,\
+        PRIMARY KEY(GAME_ID, TEAM_ID)\
+        );'
+        cursor.execute(traditional_boxscores_team_query)
+
         player_tracking_boxscores_query = 'CREATE TABLE IF NOT EXISTS player_tracking_boxscores\
         (\
         GAME_ID VARCHAR(255),\
@@ -162,31 +311,188 @@ class Db:
         );'
         cursor.execute(player_tracking_boxscores_team_query)
 
-        shots_query = 'CREATE TABLE IF NOT EXISTS shots\
+        scoring_boxscores_query = 'CREATE TABLE IF NOT EXISTS scoring_boxscores\
         (\
-        GRID_TYPE VARCHAR(255),\
         GAME_ID VARCHAR(255),\
-        GAME_EVENT_ID INT,\
+        TEAM_ID VARCHAR(255),\
+        TEAM_ABBREVIATION VARCHAR(255),\
+        TEAM_CITY VARCHAR(255),\
         PLAYER_ID VARCHAR(255),\
         PLAYER_NAME VARCHAR(255),\
+        START_POSITION VARCHAR(255),\
+        COMMENT VARCHAR(255),\
+        MIN VARCHAR(255),\
+        PCT_FGA_2PT DOUBLE,\
+        PCT_FGA_3PT DOUBLE,\
+        PCT_PTS_2PT DOUBLE,\
+        PCT_PTS_2PT_MR DOUBLE,\
+        PCT_PTS_3PT DOUBLE,\
+        PCT_PTS_FB DOUBLE,\
+        PCT_PTS_FT DOUBLE,\
+        PCT_PTS_OFF_TOV DOUBLE,\
+        PCT_PTS_PAINT DOUBLE,\
+        PCT_AST_2PM DOUBLE,\
+        PCT_UAST_2PM DOUBLE,\
+        PCT_AST_3PM DOUBLE,\
+        PCT_UAST_3PM DOUBLE,\
+        PCT_AST_FGM DOUBLE,\
+        PCT_UAST_FGM DOUBLE,\
+        PRIMARY KEY(GAME_ID, PLAYER_ID)\
+        );'
+        cursor.execute(scoring_boxscores_query)
+
+        scoring_boxscores_team_query = 'CREATE TABLE IF NOT EXISTS scoring_boxscores_team\
+        (\
+        GAME_ID VARCHAR(255),\
         TEAM_ID VARCHAR(255),\
         TEAM_NAME VARCHAR(255),\
-        PERIOD INT,\
-        MINUTES_REMAINING INT,\
-        SECONDS_REMAINING INT,\
-        EVENT_TYPE VARCHAR(255),\
-        ACTION_TYPE VARCHAR(255),\
-        SHOT_TYPE VARCHAR(255),\
-        SHOT_ZONE_BASIC VARCHAR(255),\
-        SHOT_ZONE_AREA VARCHAR(255),\
-        SHOT_ZONE_RANGE VARCHAR(255),\
-        SHOT_DISTANCE INT,\
-        LOC_X INT,\
-        LOC_Y INT,\
-        SHOT_ATTEMPTED_FLAG BOOLEAN,\
-        SHOT_MADE_FLAG BOOLEAN,\
-        PRIMARY KEY(GAME_ID, GAME_EVENT_ID)\
+        TEAM_ABBREVIATION VARCHAR(255),\
+        TEAM_CITY VARCHAR(255),\
+        MIN VARCHAR(255),\
+        PCT_FGA_2PT DOUBLE,\
+        PCT_FGA_3PT DOUBLE,\
+        PCT_PTS_2PT DOUBLE,\
+        PCT_PTS_2PT_MR DOUBLE,\
+        PCT_PTS_3PT DOUBLE,\
+        PCT_PTS_FB DOUBLE,\
+        PCT_PTS_FT DOUBLE,\
+        PCT_PTS_OFF_TOV DOUBLE,\
+        PCT_PTS_PAINT DOUBLE,\
+        PCT_AST_2PM DOUBLE,\
+        PCT_UAST_2PM DOUBLE,\
+        PCT_AST_3PM DOUBLE,\
+        PCT_UAST_3PM DOUBLE,\
+        PCT_AST_FGM DOUBLE,\
+        PCT_UAST_FGM DOUBLE,\
+        PRIMARY KEY(GAME_ID, TEAM_ID)\
         );'
-        cursor.execute(shots_query)
+        cursor.execute(scoring_boxscores_team_query)
+
+        misc_boxscores_query = 'CREATE TABLE IF NOT EXISTS misc_boxscores\
+        (\
+        GAME_ID VARCHAR(255),\
+        TEAM_ID VARCHAR(255),\
+        TEAM_ABBREVIATION VARCHAR(255),\
+        TEAM_CITY VARCHAR(255),\
+        PLAYER_ID VARCHAR(255),\
+        PLAYER_NAME VARCHAR(255),\
+        START_POSITION VARCHAR(255),\
+        COMMENT VARCHAR(255),\
+        MIN VARCHAR(255),\
+        PTS_OFF_TOV INT,\
+        PTS_2ND_CHANCE INT,\
+        PTS_FB INT,\
+        PTS_PAINT INT,\
+        OPP_PTS_OFF_TOV INT,\
+        OPP_PTS_2ND_CHANCE INT,\
+        OPP_PTS_FB INT,\
+        OPP_PTS_PAINT INT,\
+        BLK INT,\
+        BLKA INT,\
+        PF INT,\
+        PFD INT,\
+        PRIMARY KEY(GAME_ID, PLAYER_ID)\
+        );'
+        cursor.execute(misc_boxscores_query)
+
+        misc_boxscores_team_query = 'CREATE TABLE IF NOT EXISTS misc_boxscores_team\
+        (\
+        GAME_ID VARCHAR(255),\
+        TEAM_ID VARCHAR(255),\
+        TEAM_NAME VARCHAR(255),\
+        TEAM_ABBREVIATION VARCHAR(255),\
+        TEAM_CITY VARCHAR(255),\
+        MIN VARCHAR(255),\
+        PTS_OFF_TOV INT,\
+        PTS_2ND_CHANCE INT,\
+        PTS_FB INT,\
+        PTS_PAINT INT,\
+        OPP_PTS_OFF_TOV INT,\
+        OPP_PTS_2ND_CHANCE INT,\
+        OPP_PTS_FB INT,\
+        OPP_PTS_PAINT INT,\
+        BLK INT,\
+        BLKA INT,\
+        PF INT,\
+        PFD INT,\
+        PRIMARY KEY(GAME_ID, TEAM_ID)\
+        );'
+        cursor.execute(misc_boxscores_team_query)
+
+        usage_boxscores_query = 'CREATE TABLE IF NOT EXISTS usage_boxscores\
+        (\
+        GAME_ID VARCHAR(255),\
+        TEAM_ID VARCHAR(255),\
+        TEAM_ABBREVIATION VARCHAR(255),\
+        TEAM_CITY VARCHAR(255),\
+        PLAYER_ID VARCHAR(255),\
+        PLAYER_NAME VARCHAR(255),\
+        START_POSITION VARCHAR(255),\
+        COMMENT VARCHAR(255),\
+        MIN VARCHAR(255),\
+        USG_PCT DOUBLE,\
+        PCT_FGM DOUBLE,\
+        PCT_FGA DOUBLE,\
+        PCT_FG3M DOUBLE,\
+        PCT_FG3A DOUBLE,\
+        PCT_FTM DOUBLE,\
+        PCT_FTA DOUBLE,\
+        PCT_OREB DOUBLE,\
+        PCT_DREB DOUBLE,\
+        PCT_REB DOUBLE,\
+        PCT_AST DOUBLE,\
+        PCT_TOV DOUBLE,\
+        PCT_STL DOUBLE,\
+        PCT_BLK DOUBLE,\
+        PCT_BLKA DOUBLE,\
+        PCT_PF DOUBLE,\
+        PCT_PFD DOUBLE,\
+        PCT_PTS DOUBLE,\
+        PRIMARY KEY(GAME_ID, PLAYER_ID)\
+        );'
+        cursor.execute(usage_boxscores_query)
+
+        four_factors_boxscores_query = 'CREATE TABLE IF NOT EXISTS four_factors_boxscores\
+        (\
+        GAME_ID VARCHAR(255),\
+        TEAM_ID VARCHAR(255),\
+        TEAM_ABBREVIATION VARCHAR(255),\
+        TEAM_CITY VARCHAR(255),\
+        PLAYER_ID VARCHAR(255),\
+        PLAYER_NAME VARCHAR(255),\
+        START_POSITION VARCHAR(255),\
+        COMMENT VARCHAR(255),\
+        MIN VARCHAR(255),\
+        EFG_PCT DOUBLE,\
+        FTA_RATE DOUBLE,\
+        TM_TOV_PCT DOUBLE,\
+        OREB_PCT DOUBLE,\
+        OPP_EFG_PCT DOUBLE,\
+        OPP_FTA_RATE DOUBLE,\
+        OPP_TOV_PCT DOUBLE,\
+        OPP_OREB_PCT DOUBLE,\
+        PRIMARY KEY(GAME_ID, PLAYER_ID)\
+        );'
+        cursor.execute(four_factors_boxscores_query)
+
+        four_factors_boxscores_team_query = 'CREATE TABLE IF NOT EXISTS four_factors_boxscores_team\
+        (\
+        GAME_ID VARCHAR(255),\
+        TEAM_ID VARCHAR(255),\
+        TEAM_NAME VARCHAR(255),\
+        TEAM_ABBREVIATION VARCHAR(255),\
+        TEAM_CITY VARCHAR(255),\
+        MIN VARCHAR(255),\
+        EFG_PCT DOUBLE,\
+        FTA_RATE DOUBLE,\
+        TM_TOV_PCT DOUBLE,\
+        OREB_PCT DOUBLE,\
+        OPP_EFG_PCT DOUBLE,\
+        OPP_FTA_RATE DOUBLE,\
+        OPP_TOV_PCT DOUBLE,\
+        OPP_OREB_PCT DOUBLE,\
+        PRIMARY KEY(GAME_ID, TEAM_ID)\
+        );'
+        cursor.execute(four_factors_boxscores_team_query)
 
         conn.close()
