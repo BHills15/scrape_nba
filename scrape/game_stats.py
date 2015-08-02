06931169e638c80e04d3d58d5719ce41c6d30e0e
dblackrun <blackport99@yahoo.com>
2015-08-02 12:28:29 -0400

    cleaning up names

--- scrape/game_stats.py
+++ scrape/game_stats.py
@@ -5,39 +5,39 @@ import pandas as pd
 class GameData:
     def __init__(self, game_id):
         self.game_id = game_id
-        self._pbp_url = "http://stats.nba.com/stats/playbyplayv2?EndPeriod=10&EndRange=55800&GameID=" + game_id + "&RangeType=2&Season=2014-15&SeasonType=Regular+Season&StartPeriod=1&StartRange=0"
-        self._pbp_response = urllib2.urlopen(self._pbp_url)
-        self._pbp_data = json.loads(self._pbp_response.read())
+        self.pbp_url = "http://stats.nba.com/stats/playbyplayv2?EndPeriod=10&EndRange=55800&GameID=" + game_id + "&RangeType=2&Season=2014-15&SeasonType=Regular+Season&StartPeriod=1&StartRange=0"
+        self.pbp_response = urllib2.urlopen(self.pbp_url)
+        self.pbp_data = json.loads(self.pbp_response.read())
 
-        self._player_tracking_boxscore_url = "http://stats.nba.com/stats/boxscoreplayertrackv2?GameId="+game_id
-        self._player_tracking_boxscore_response = urllib2.urlopen(self._player_tracking_boxscore_url)
-        self._player_tracking_boxscore_data = json.loads(self._player_tracking_boxscore_response.read())
+        self.player_tracking_boxscore_url = "http://stats.nba.com/stats/boxscoreplayertrackv2?GameId="+game_id
+        self.player_tracking_boxscore_response = urllib2.urlopen(self.player_tracking_boxscore_url)
+        self.player_tracking_boxscore_data = json.loads(self.player_tracking_boxscore_response.read())
 
-        self._teams = [self.player_tracking_boxscore_team()[0]['TEAM_ID'], self.player_tracking_boxscore_team()[1]['TEAM_ID']]
+        self.teams = [self.player_tracking_boxscore_team()[0]['TEAM_ID'], self.player_tracking_boxscore_team()[1]['TEAM_ID']]
 
     def pbp(self):
-        _headers = self._pbp_data['resultSets'][0]['headers']
-        _rows = self._pbp_data['resultSets'][0]['rowSet']
-        return [dict(zip(_headers, row)) for row in _rows]
+        headers = self.pbp_data['resultSets'][0]['headers']
+        rows = self.pbp_data['resultSets'][0]['rowSet']
+        return [dict(zip(headers, row)) for row in rows]
 
     def player_tracking_boxscore(self):
-        _headers = self._player_tracking_boxscore_data['resultSets'][0]['headers']
-        _rows = self._player_tracking_boxscore_data['resultSets'][0]['rowSet']
-        return [dict(zip(_headers, row)) for row in _rows]
+        headers = self.player_tracking_boxscore_data['resultSets'][0]['headers']
+        rows = self.player_tracking_boxscore_data['resultSets'][0]['rowSet']
+        return [dict(zip(headers, row)) for row in rows]
 
     def player_tracking_boxscore_team(self):
-        _headers = self._player_tracking_boxscore_data['resultSets'][1]['headers']
-        _rows = self._player_tracking_boxscore_data['resultSets'][1]['rowSet']
-        return [dict(zip(_headers, row)) for row in _rows]
+        headers = self.player_tracking_boxscore_data['resultSets'][1]['headers']
+        rows = self.player_tracking_boxscore_data['resultSets'][1]['rowSet']
+        return [dict(zip(headers, row)) for row in rows]
 
 
     def shots(self):
-        _shots = []
-        for team in self._teams:
-            _shots_url = "http://stats.nba.com/stats/shotchartdetail?Season=2014-15&SeasonType=Regular+Season&LeagueID=00&TeamID="+str(team)+"&PlayerID=0&GameID="+self.game_id+"&Outcome=&Location=&Month=0&SeasonSegment=&DateFrom=&DateTo=&OpponentTeamID=0&VsConference=&VsDivision=&Position=&RookieYear=&GameSegment=&Period=0&LastNGames=0&ContextFilter=&ContextMeasure=FG_PCT&display-mode=performance&zone-mode=zone&zoneOverlays=false&zoneDetails=false&viewShots=true"
-            _shots_response = urllib2.urlopen(_shots_url)
-            _shots_data = json.loads(_shots_response.read())
-            _headers = _shots_data['resultSets'][0]['headers']
-            _rows = _shots_data['resultSets'][0]['rowSet']
-            _shots += [dict(zip(_headers, row)) for row in _rows]
-        return _shots
+        shots = []
+        for team in self.teams:
+            shots_url = "http://stats.nba.com/stats/shotchartdetail?Season=2014-15&SeasonType=Regular+Season&LeagueID=00&TeamID="+str(team)+"&PlayerID=0&GameID="+self.game_id+"&Outcome=&Location=&Month=0&SeasonSegment=&DateFrom=&DateTo=&OpponentTeamID=0&VsConference=&VsDivision=&Position=&RookieYear=&GameSegment=&Period=0&LastNGames=0&ContextFilter=&ContextMeasure=FG_PCT&display-mode=performance&zone-mode=zone&zoneOverlays=false&zoneDetails=false&viewShots=true"
+            shots_response = urllib2.urlopen(shots_url)
+            shots_data = json.loads(shots_response.read())
+            headers = shots_data['resultSets'][0]['headers']
+            rows = shots_data['resultSets'][0]['rowSet']
+            shots += [dict(zip(headers, row)) for row in rows]
+        return shots
--- scrape/player_stats.py
+++ scrape/player_stats.py
@@ -5,12 +5,12 @@ class PlayerData:
     def __init__(self, player_id):
         self.player_id = player_id
 
-        self._shot_logs_url = base_url = "http://stats.nba.com/stats/playerdashptshotlog?DateFrom=&DateTo=&GameSegment=&LastNGames=0&LeagueID=00&Location=&Month=0&OpponentTeamID=0&Outcome=&Period=0&PlayerID="+player_id+"&Season=2014-15&SeasonSegment=&SeasonType=Regular+Season&TeamID=0&VsConference=&VsDivision="
-        self._shot_logs_response = urllib2.urlopen(self._shot_logs_url)
-        self._shot_logs_data = json.loads(self._shot_logs_response.read())
+        self.shot_logs_url = base_url = "http://stats.nba.com/stats/playerdashptshotlog?DateFrom=&DateTo=&GameSegment=&LastNGames=0&LeagueID=00&Location=&Month=0&OpponentTeamID=0&Outcome=&Period=0&PlayerID="+player_id+"&Season=2014-15&SeasonSegment=&SeasonType=Regular+Season&TeamID=0&VsConference=&VsDivision="
+        self.shot_logs_response = urllib2.urlopen(self.shot_logs_url)
+        self.shot_logs_data = json.loads(self.shot_logs_response.read())
 
     def shot_logs(self):
         # won't have player id
-        _headers = self._pbp_data['resultSets'][0]['headers']
-        _rows = self._pbp_data['resultSets'][0]['rowSet']
-        return [dict(zip(_headers, row)) for row in _rows]
+        headers = self.pbp_data['resultSets'][0]['headers']
+        rows = self.pbp_data['resultSets'][0]['rowSet']
+        return [dict(zip(headers, row)) for row in rows]
