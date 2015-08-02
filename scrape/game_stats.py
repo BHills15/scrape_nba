07b03ffecd38e11e1f8675ddfaeb11ebdab5cd2c
dblackrun <blackport99@yahoo.com>
2015-08-02 13:55:57 -0400

    added all boxscore stats

--- scrape/game_stats.py
+++ scrape/game_stats.py
@@ -18,10 +18,41 @@ class GameData:
         self.traditional_boxscore_response = urllib2.urlopen(self.traditional_boxscore_url)
         self.traditional_boxscore_data = json.loads(self.traditional_boxscore_response.read())
 
+        self.advanced_boxscore_url = "http://stats.nba.com/stats/boxscoreadvancedv2?GameId="+game_id+"&StartPeriod=0&EndPeriod=10&RangeType=2&StartRange=0&EndRange=55800"
+        self.advanced_boxscore_response = urllib2.urlopen(self.advanced_boxscore_url)
+        self.advanced_boxscore_data = json.loads(self.advanced_boxscore_response.read())
+
+        self.scoring_boxscore_url = "http://stats.nba.com/stats/boxscorescoringv2?GameId="+game_id+"&StartPeriod=0&EndPeriod=10&RangeType=2&StartRange=0&EndRange=55800"
+        self.scoring_boxscore_response = urllib2.urlopen(self.scoring_boxscore_url)
+        self.scoring_boxscore_data = json.loads(self.scoring_boxscore_response.read())
+
+        self.misc_boxscore_url = "http://stats.nba.com/stats/boxscoremiscv2?GameId="+game_id+"&StartPeriod=0&EndPeriod=10&RangeType=2&StartRange=0&EndRange=55800"
+        self.misc_boxscore_response = urllib2.urlopen(self.misc_boxscore_url)
+        self.misc_boxscore_data = json.loads(self.misc_boxscore_response.read())
+
+        self.usage_boxscore_url = "http://stats.nba.com/stats/boxscoreusagev2?GameId="+game_id+"&StartPeriod=0&EndPeriod=10&RangeType=2&StartRange=0&EndRange=55800"
+        self.usage_boxscore_response = urllib2.urlopen(self.usage_boxscore_url)
+        self.usage_boxscore_data = json.loads(self.usage_boxscore_response.read())
+
+        self.four_factors_boxscore_url = "http://stats.nba.com/stats/boxscorefourfactorsv2?GameId="+game_id+"&StartPeriod=0&EndPeriod=10&RangeType=2&StartRange=0&EndRange=55800"
+        self.four_factors_boxscore_response = urllib2.urlopen(self.four_factors_boxscore_url)
+        self.four_factors_boxscore_data = json.loads(self.four_factors_boxscore_response.read())
+
     def pbp(self):
         headers = self.pbp_data['resultSets'][0]['headers']
         rows = self.pbp_data['resultSets'][0]['rowSet']
         return [dict(zip(headers, row)) for row in rows]
+
+    def shots(self):
+        game_shots = []
+        for team in self.teams:
+            shots_url = "http://stats.nba.com/stats/shotchartdetail?Season=2014-15&SeasonType=Regular+Season&LeagueID=00&TeamID="+str(team)+"&PlayerID=0&GameID="+self.game_id+"&Outcome=&Location=&Month=0&SeasonSegment=&DateFrom=&DateTo=&OpponentTeamID=0&VsConference=&VsDivision=&Position=&RookieYear=&GameSegment=&Period=0&LastNGames=0&ContextFilter=&ContextMeasure=FG_PCT&display-mode=performance&zone-mode=zone&zoneOverlays=false&zoneDetails=false&viewShots=true"
+            shots_response = urllib2.urlopen(shots_url)
+            shots_data = json.loads(shots_response.read())
+            headers = shots_data['resultSets'][0]['headers']
+            rows = shots_data['resultSets'][0]['rowSet']
+            game_shots += [dict(zip(headers, row)) for row in rows]
+        return game_shots
 
     def player_tracking_boxscore(self):
         headers = self.player_tracking_boxscore_data['resultSets'][0]['headers']
@@ -33,17 +64,6 @@ class GameData:
         rows = self.player_tracking_boxscore_data['resultSets'][1]['rowSet']
         return [dict(zip(headers, row)) for row in rows]
 
-    def shots(self):
-        shots = []
-        for team in self.teams:
-            shots_url = "http://stats.nba.com/stats/shotchartdetail?Season=2014-15&SeasonType=Regular+Season&LeagueID=00&TeamID="+str(team)+"&PlayerID=0&GameID="+self.game_id+"&Outcome=&Location=&Month=0&SeasonSegment=&DateFrom=&DateTo=&OpponentTeamID=0&VsConference=&VsDivision=&Position=&RookieYear=&GameSegment=&Period=0&LastNGames=0&ContextFilter=&ContextMeasure=FG_PCT&display-mode=performance&zone-mode=zone&zoneOverlays=false&zoneDetails=false&viewShots=true"
-            shots_response = urllib2.urlopen(shots_url)
-            shots_data = json.loads(shots_response.read())
-            headers = shots_data['resultSets'][0]['headers']
-            rows = shots_data['resultSets'][0]['rowSet']
-            shots += [dict(zip(headers, row)) for row in rows]
-        return shots
-
     def traditional_boxscore(self):
         headers = self.traditional_boxscore_data['resultSets'][0]['headers']
         rows = self.traditional_boxscore_data['resultSets'][0]['rowSet']
@@ -52,4 +72,54 @@ class GameData:
     def traditional_boxscore_team(self):
         headers = self.traditional_boxscore_data['resultSets'][1]['headers']
         rows = self.traditional_boxscore_data['resultSets'][1]['rowSet']
+        return [dict(zip(headers, row)) for row in rows]
+
+    def advanced_boxscore(self):
+        headers = self.advanced_boxscore_data['resultSets'][0]['headers']
+        rows = self.advanced_boxscore_data['resultSets'][0]['rowSet']
+        return [dict(zip(headers, row)) for row in rows]
+
+    def advanced_boxscore_team(self):
+        headers = self.advanced_boxscore_data['resultSets'][1]['headers']
+        rows = self.advanced_boxscore_data['resultSets'][1]['rowSet']
+        return [dict(zip(headers, row)) for row in rows]
+
+    def scoring_boxscore(self):
+        headers = self.scoring_boxscore_data['resultSets'][0]['headers']
+        rows = self.scoring_boxscore_data['resultSets'][0]['rowSet']
+        return [dict(zip(headers, row)) for row in rows]
+
+    def scoring_boxscore_team(self):
+        headers = self.scoring_boxscore_data['resultSets'][1]['headers']
+        rows = self.scoring_boxscore_data['resultSets'][1]['rowSet']
+        return [dict(zip(headers, row)) for row in rows]
+
+    def misc_boxscore(self):
+        headers = self.misc_boxscore_data['resultSets'][0]['headers']
+        rows = self.misc_boxscore_data['resultSets'][0]['rowSet']
+        return [dict(zip(headers, row)) for row in rows]
+
+    def misc_boxscore_team(self):
+        headers = self.misc_boxscore_data['resultSets'][1]['headers']
+        rows = self.misc_boxscore_data['resultSets'][1]['rowSet']
+        return [dict(zip(headers, row)) for row in rows]
+
+    def usage_boxscore(self):
+        headers = self.usage_boxscore_data['resultSets'][0]['headers']
+        rows = self.usage_boxscore_data['resultSets'][0]['rowSet']
+        return [dict(zip(headers, row)) for row in rows]
+
+    def usage_boxscore_team(self):
+        headers = self.usage_boxscore_data['resultSets'][1]['headers']
+        rows = self.usage_boxscore_data['resultSets'][1]['rowSet']
+        return [dict(zip(headers, row)) for row in rows]
+
+    def four_factors_boxscore(self):
+        headers = self.four_factors_boxscore_data['resultSets'][0]['headers']
+        rows = self.four_factors_boxscore_data['resultSets'][0]['rowSet']
+        return [dict(zip(headers, row)) for row in rows]
+
+    def four_factors_boxscore_team(self):
+        headers = self.four_factors_boxscore_data['resultSets'][1]['headers']
+        rows = self.four_factors_boxscore_data['resultSets'][1]['rowSet']
         return [dict(zip(headers, row)) for row in rows]
