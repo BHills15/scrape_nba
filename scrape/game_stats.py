c476231dd0a29cd4b067e2c289e03aab2eb6b08b
dblackrun <blackport99@yahoo.com>
2015-08-02 13:58:55 -0400

    cleanup

--- scrape/game_stats.py
+++ scrape/game_stats.py
@@ -4,6 +4,7 @@ import urllib2
 class GameData:
     def __init__(self, game_id):
         self.game_id = game_id
+        
         self.pbp_url = "http://stats.nba.com/stats/playbyplayv2?EndPeriod=10&EndRange=55800&GameID=" + game_id + "&RangeType=2&Season=2014-15&SeasonType=Regular+Season&StartPeriod=1&StartRange=0"
         self.pbp_response = urllib2.urlopen(self.pbp_url)
         self.pbp_data = json.loads(self.pbp_response.read())
@@ -11,8 +12,6 @@ class GameData:
         self.player_tracking_boxscore_url = "http://stats.nba.com/stats/boxscoreplayertrackv2?GameId="+game_id
         self.player_tracking_boxscore_response = urllib2.urlopen(self.player_tracking_boxscore_url)
         self.player_tracking_boxscore_data = json.loads(self.player_tracking_boxscore_response.read())
-
-        self.teams = [self.player_tracking_boxscore_team()[0]['TEAM_ID'], self.player_tracking_boxscore_team()[1]['TEAM_ID']]
 
         self.traditional_boxscore_url = "http://stats.nba.com/stats/boxscoretraditionalv2?GameId="+game_id+"&StartPeriod=0&EndPeriod=10&RangeType=2&StartRange=0&EndRange=55800"
         self.traditional_boxscore_response = urllib2.urlopen(self.traditional_boxscore_url)
@@ -37,6 +36,8 @@ class GameData:
         self.four_factors_boxscore_url = "http://stats.nba.com/stats/boxscorefourfactorsv2?GameId="+game_id+"&StartPeriod=0&EndPeriod=10&RangeType=2&StartRange=0&EndRange=55800"
         self.four_factors_boxscore_response = urllib2.urlopen(self.four_factors_boxscore_url)
         self.four_factors_boxscore_data = json.loads(self.four_factors_boxscore_response.read())
+
+        self.teams = [self.player_tracking_boxscore_team()[0]['TEAM_ID'], self.player_tracking_boxscore_team()[1]['TEAM_ID']]
 
     def pbp(self):
         headers = self.pbp_data['resultSets'][0]['headers']
