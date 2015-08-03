7830a95dd4dfa69a0e823efa88a146a2554e2e57
dblackrun <blackport99@yahoo.com>
2015-08-02 21:24:06 -0400

    fixing urls

--- scrape/sportvu_stats.py
+++ scrape/sportvu_stats.py
@@ -39,39 +39,39 @@ class SportVuData:
         self.catch_shoot_team_response = urllib2.urlopen(self.catch_shoot_team_url)
         self.catch_shoot_team_data = json.loads(self.catch_shoot_team_response.read())
 
-        self.touches_url = "http://stats.nba.com/js/data/sportvu/2014/touchesTeamData.json"
+        self.touches_url = "http://stats.nba.com/js/data/sportvu/2014/touchesData.json"
         self.touches_response = urllib2.urlopen(self.touches_url)
         self.touches_data = json.loads(self.touches_response.read())
 
-        self.defense_url = "http://stats.nba.com/js/data/sportvu/2014/defenseTeamData.json"
+        self.defense_url = "http://stats.nba.com/js/data/sportvu/2014/defenseData.json"
         self.defense_response = urllib2.urlopen(self.defense_url)
         self.defense_data = json.loads(self.defense_response.read())
 
-        self.drives_url = "http://stats.nba.com/js/data/sportvu/2014/drivesTeamData.json"
+        self.drives_url = "http://stats.nba.com/js/data/sportvu/2014/drivesData.json"
         self.drives_response = urllib2.urlopen(self.drives_url)
         self.drives_data = json.loads(self.drives_response.read())
 
-        self.passing_url = "http://stats.nba.com/js/data/sportvu/2014/passingTeamData.json"
+        self.passing_url = "http://stats.nba.com/js/data/sportvu/2014/passingData.json"
         self.passing_response = urllib2.urlopen(self.passing_url)
         self.passing_data = json.loads(self.passing_response.read())
 
-        self.pull_up_shoot_url = "http://stats.nba.com/js/data/sportvu/2014/pullUpShootTeamData.json"
+        self.pull_up_shoot_url = "http://stats.nba.com/js/data/sportvu/2014/pullUpShootData.json"
         self.pull_up_shoot_response = urllib2.urlopen(self.pull_up_shoot_url)
         self.pull_up_shoot_data = json.loads(self.pull_up_shoot_response.read())
 
-        self.rebounding_url = "http://stats.nba.com/js/data/sportvu/2014/reboundingTeamData.json"
+        self.rebounding_url = "http://stats.nba.com/js/data/sportvu/2014/reboundingData.json"
         self.rebounding_response = urllib2.urlopen(self.rebounding_url)
         self.rebounding_data = json.loads(self.rebounding_response.read())
 
-        self.shooting_url = "http://stats.nba.com/js/data/sportvu/2014/shootingTeamData.json"
+        self.shooting_url = "http://stats.nba.com/js/data/sportvu/2014/shootingData.json"
         self.shooting_response = urllib2.urlopen(self.shooting_url)
         self.shooting_data = json.loads(self.shooting_response.read())
 
-        self.speed_url = "http://stats.nba.com/js/data/sportvu/2014/speedTeamData.json"
+        self.speed_url = "http://stats.nba.com/js/data/sportvu/2014/speedData.json"
         self.speed_response = urllib2.urlopen(self.speed_url)
         self.speed_data = json.loads(self.speed_response.read())
 
-        self.catch_shoot_url = "http://stats.nba.com/js/data/sportvu/2014/catchShootTeamData.json"
+        self.catch_shoot_url = "http://stats.nba.com/js/data/sportvu/2014/catchShootData.json"
         self.catch_shoot_response = urllib2.urlopen(self.catch_shoot_url)
         self.catch_shoot_data = json.loads(self.catch_shoot_response.read())
 
