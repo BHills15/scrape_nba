2f03ef6651219011019f419edc98f4d369e666cf
dblackrun <blackport99@yahoo.com>
2015-08-03 21:06:54 -0400

    added function to get game ids for date"

--- scrape/helper.py
+++ scrape/helper.py
@@ -24,3 +24,13 @@ def get_data_from_url_rename_columns(url, renamed_columns, index):
         headers = [renamed_columns[key] if x==key else x for x in headers]
     rows = data['resultSets'][index]['rowSet']
     return [dict(zip(headers, row)) for row in rows]
+
+def get_game_ids_for_date(date):
+    # date format YYYY-MM-DD
+    game_ids = []
+    split = date.split("-")
+    url = "http://stats.nba.com/stats/scoreboardV2?DayOffset=0&LeagueID=00&gameDate="+split[1]+"%2F"+split[2]+"%2F"+split[0]
+    games = get_data_from_url(url, 1)
+    for game in games:
+        game_ids.append(game['GAME_ID'])
+    return game_ids
